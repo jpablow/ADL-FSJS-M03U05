fc4bec4c108c1b2e1429b6f5d7bc70b3ce1b85d4
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-03-13 02:44:12 -0300

    Crea Fx setDate, mejora en CSS y HTML

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,6 +2,11 @@ const taskList = [{id: 1, name: "Ir al supermercado", done: true}, {id: 2, name:
 var taskList_doneF = [];
 var taskList_doneT = [];
 
+const dateName = document.querySelector("#dayName");
+const dateDay = document.querySelector("#day");
+const dateMonth = document.querySelector("#month");
+const dateYear = document.querySelector("#year");
+
 var input = document.querySelector("#input");
 var btnAdd = document.querySelector("#btnAdd");
 var doneF = document.querySelector("#doneF");
@@ -10,6 +15,14 @@ var totalTasks = document.querySelector("#total_tasks");
 var totalDone = document.querySelector("#total_done");
 var html = "";
 var i = 3;
+
+function setDate () {
+    const date = new Date();
+    dateName.innerHTML = date.toLocaleString("es", {weekday: "long"});
+    dateDay.innerHTML = date.toLocaleString("es", {day: "numeric"});
+    dateMonth.innerHTML = date.toLocaleString("es", {month: "long"});
+    dateYear.innerHTML = date.toLocaleString("es", {year: "numeric"});
+};
 
 function taskListRefresh () {
     doneF.innerHTML = "";
@@ -26,25 +39,25 @@ function taskListRefresh () {
                 <div class="task_ok btn"><img src="assets/img/caja.png" class="btn_img" id="btn_status" alt="Terminado" onclick="changeTaskStatus(${A.id})"></div>
                 <div class="task_delete btn"><img src="assets/img/basura.png" class="btn_img" id="btn_delete" alt="Eliminar" onclick="deleteTask(${A.id})"></div>
             </div>
-        `
+        `;
     });
     doneT.innerHTML = `<h3>Tareas terminadas</h3>${html}`;
     html = "";
     taskList_doneF.forEach(A => {
         html +=
         `
-                <div class="task">
-                    <div class="task_id">${A.id}</div>
-                    <div class="task_name">${A.name}</div>
-                    <div class="task_notOk btn"><img src="assets/img/cuadrado.png" class="btn_img" id="btn_status" alt="Pendiente" onclick="changeTaskStatus(${A.id})"></div>
-                    <div class="task_delete btn"><img src="assets/img/basura.png" class="btn_img" id="btn_delete" alt="Eliminar" onclick="deleteTask(${A.id})"></div>
-                </div>
-        `
+            <div class="task">
+                <div class="task_id">${A.id}</div>
+                <div class="task_name">${A.name}</div>
+                <div class="task_notOk btn"><img src="assets/img/cuadrado.png" class="btn_img" id="btn_status" alt="Pendiente" onclick="changeTaskStatus(${A.id})"></div>
+                <div class="task_delete btn"><img src="assets/img/basura.png" class="btn_img" id="btn_delete" alt="Eliminar" onclick="deleteTask(${A.id})"></div>
+            </div>
+        `;
     });
     doneF.innerHTML = `<h3>Tareas pendientes</h3>${html}`;
     totalDone.innerHTML = taskList_doneT.length;
     totalTasks.innerHTML = taskList_doneF.length + taskList_doneT.length;
-}
+};
 
 function addNewTask () {
     if (input.value == "") {
@@ -54,15 +67,15 @@ function addNewTask () {
         taskList.push({id: i, name: input.value, done: false});
         input.value = "";
         taskListRefresh();
-    }
-}
+    };
+};
 
 function deleteTask (idT) {
     console.log(idT);
     let indexDelete = taskList.findIndex(T => T.id == idT);
     taskList.splice(indexDelete, 1,);
     taskListRefresh();
-}
+};
 
 function changeTaskStatus (idT) {
     let indexChange = taskList.findIndex(T => T.id == idT);
@@ -70,9 +83,10 @@ function changeTaskStatus (idT) {
         taskList[indexChange].done = true;
     } else {
         taskList[indexChange].done = false;
-    }
+    };
     taskListRefresh();
-}
+};
 
 window.addEventListener("load", taskListRefresh);
+window.addEventListener("load", setDate);
 btnAdd.addEventListener("click", addNewTask);
